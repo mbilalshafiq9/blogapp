ca73f0b495824f4ca1391a11cf8ec4475f8a3af4
mbilalshafiq9 <53928252+mbilalshafiq9@users.noreply.github.com>
2021-07-17 21:11:10 +0500

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)te4cfve#q@2f$#+mscn+vm@#i&2em0dsosy0oj4^r$agy$*mg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','blogtoglob.herokuapp.com']
 
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
