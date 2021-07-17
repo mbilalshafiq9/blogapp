7a27e540362afe29141b96fdf590c86fa91dac4f
mbilalshafiq9 <53928252+mbilalshafiq9@users.noreply.github.com>
2021-07-17 19:41:29 +0500

    Add files via upload

--- Blog/settings.py
+++ Blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)te4cfve#q@2f$#+mscn+vm@#i&2em0dsosy0oj4^r$agy$*mg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
