cd5d62d461ea0d7d68dce0f35ea1d5a5707fa8e5
mbilalshafiq9 <53928252+mbilalshafiq9@users.noreply.github.com>
2021-07-18 11:35:50 +0500

    Update urls.py

--- Blog/urls.py
+++ Blog/urls.py
@@ -17,7 +17,8 @@ from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from django.conf.urls.static import static,url
+from django.conf.urls.static import static
+from django.conf.urls import url
 from django.views.static import serve
 
 urlpatterns = [
